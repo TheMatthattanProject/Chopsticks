1965c46f47104ff5de2207650d0ebc53bb88b6f8
Matthew <57062385+TheMatthattanProject@users.noreply.github.com>
2022-10-02 19:02:23 +1100

    Lazy commit to transfer to laptop.

--- Chopsticks/Individual.cpp
+++ Chopsticks/Individual.cpp
@@ -1,3 +1,50 @@
+#define AMOUNT_OF_HANDS 2
+
+using namespace std;
+
+class Individual
+{
+	public:
+		int hands[AMOUNT_OF_HANDS];
+		Individual();
+		bool aliveNextRound(Individual &opponent);
+};
 
 
-class 
+Individual::Individual() 
+{
+	int length = sizeof(this->hands) / sizeof(this->hands[0]);
+	for (int i = 0; i < length; i++) 
+	{
+		this->hands[i] = 1;
+	}
+}
+
+
+bool Individual::aliveNextRound(Individual& opponent) 
+{
+	// Check that exactly one hand is left 
+	int length = sizeof(this->hands) / sizeof(this->hands[0]);
+	int handsLeft = 0;
+	int lastHandPos = 0;
+	for (int i = 0; i < length; i++) 
+	{
+		if (this->hands[i] != 0)
+		{
+			handsLeft++;
+		}
+
+		if (handsLeft > 1)
+		{
+			return true;
+		}
+
+	}
+
+
+
+	// 
+
+
+
+}
